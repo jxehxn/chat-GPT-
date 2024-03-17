e9bcbd1ae7b6c738c1d3d326a32b2f8e3d3516ff
jxehxn <wkdwogns2345@naver.com>
2024-03-17 15:52:44 +0900

    -

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const OpenAI = require('openai');
 var cors = require('cors')
 const openai = new OpenAI({
-  apiKey: "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", // This is the default and can be omitted
+  apiKey: "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", // This is the default and can be omitted
 });
 
 const express = require('express')
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing
 // })
 
 // POST method route
-app.post('/fortuneTell', async function(req, res) {
+app.post('/fortuneTell', async (req, res) => {
     const Completion = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: [{ role: 'system', content: '당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다.' },
